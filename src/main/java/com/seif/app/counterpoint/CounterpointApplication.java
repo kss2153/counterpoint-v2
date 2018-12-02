62bbc20eac51e99a78758113177094ef8a070a8e
Kayvon Seif-Naraghi <kss2153@columbia.edu>
2018-12-02 11:00:23 -0500

    initial commit

---
+++ src/main/java/com/seif/app/counterpoint/CounterpointApplication.java
@@ -0,0 +1,20 @@
+package com.seif.app.counterpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CounterpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounterpointApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "Welcome!";
+	}
+}
